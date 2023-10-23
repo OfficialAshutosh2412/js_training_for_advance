de989b3de3c18bd21bc612feab6ae79eade29987
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-10-24 00:46:40 +0530

    variable and scopes

---
+++ day1/d1.js
@@ -0,0 +1,24 @@
+// console.log("hello world !");
+
+// var a="hi";
+// console.log(a);
+// a="bye";
+// console.log(a);
+// console.log("------------------------");
+
+// let b="yes";
+// console.log(b);
+// b="3";
+// console.log(b);
+// console.log("------------------------");
+
+// const c="kal";
+// console.log(c);
+// c="aaj";
+// console.log(c);
+// console.log("------------------------");
+
+// var yes="lodu";
+// let yes="ludo";
+// console.log(yes,yes);
+// var is  function scoped but let is block scoped
