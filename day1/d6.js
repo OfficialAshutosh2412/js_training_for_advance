72fe1a0073e6492116f0d31c29a7157b93837c5f
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-10-26 22:15:22 +0530

    functions

---
+++ day1/d6.js
@@ -0,0 +1,42 @@
+// functions
+
+// without params
+// function add(){
+//     let a=10,b=10;
+//     console.log(a+b);
+// }
+// add();
+
+// with argument and param
+// params
+// function add(a, b){
+//     console.log(a+b);
+// }
+//  add(160,10)//arguments
+
+// what is function expression?
+// simply make a function, and store it into a variable before calling, and write only variable name to call it.
+// function mod(a,b){
+//     console.log(a%b);
+// }
+// let func=mod(10,4);
+// func;calling function using variable name
+// function expression in incomplete without return keyword.
+
+// for completion, we have to return something from function and pass into the variable which stores function call.
+
+// function div(a,b){
+//     return a/b;
+// }
+// let yupp=div(12,5);
+// console.log(yupp);
+// this is complete function expression
+
+// whats an anonymous function, and anonymous function expression?
+// a function without a name called anonymous function and an anonymous function is stored in a variable and called using variable name called AFE.
+
+// let funName=function(a,b){
+//     return a+b;
+// }
+// console.log(typeof funName);
+// console.log(funName(10,45));
