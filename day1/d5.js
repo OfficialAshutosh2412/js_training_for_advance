c30c5e1a48d6c9812903c3184b52889fe09dba28
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-10-26 14:44:51 +0530

    loops

---
+++ day1/d5.js
@@ -0,0 +1,86 @@
+// control statement
+// if-else
+// let today="rain";
+// if(today=="rain"){
+//     console.log("holiday");
+// }else{
+//     console.log("go to school");
+// }
+
+// leap year
+// let year=2008;
+// if(year%4===0){
+//     if(year%100==0){
+//         console.log("non-leap");
+//         if(year%400==0){
+//             console.log("leap");
+//         }else{
+//             console.log("non-leap");
+//         }
+//     }else{
+//         console.log("leap");
+//     }
+// }
+// else{
+//     console.log("non-leap");
+// }
+
+// what is truthy and falsy in js
+// 0,null-undefined,falsy,false,NaN are the falsy values which is always be a fals value in any condition and others ar truthy values.
+// if(null){
+//     console.log("lost");
+// }else{
+//     console.log("won");
+// }
+// if(undefined){
+//     console.log("lost");
+// }else{
+//     console.log("won");
+// }
+
+// ternary operator
+// let age=17;
+// age=(age<=18)?console.log("eligible"):console.console.log("not eligible");
+
+// let weather="rainy";
+// weather=(weather!="rainy")?console.log("rainy day"):console.log("sunny day");
+
+// switch statement
+// let a=5,b=4,c=0;
+// let opr="add"
+
+// switch(opr){
+//     case 'add':
+//         console.log(a+b);
+//         break;
+//     case 'sub':
+//         console.log(a-b);
+//         break;
+//     case 'mul':
+//         console.log(a*b);
+//         break;
+//     case 'div':
+//         console.log(a/b);
+//         break;
+//     case 'mod':
+//         console.log(a%b);
+//         break;
+//     default:
+//         console.log("invalid option");
+// }
+
+// while loop
+// let a=1;
+// while(a<=10){
+//     console.log(`2 x ${a} = ${2*a}`);
+//     a++;
+// }
+
+// for loop
+// for(let i=1;i<=10;i++){
+//     console.log(`2 x ${i} = ${2*i}`);
+// }
+// let findTableOf=7;
+// for(let i=1;i<=10;i++){
+//     console.log(`${findTableOf} x ${i} = ${findTableOf*i}`);
+// }
