a96db8499965087f8001d4d7bc7c74d5ef02ee8a
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-11-07 21:11:01 +0530

    dom intro

---
+++ day1/domConsole.js
@@ -0,0 +1,10 @@
+// change color of id
+// let a =document.getElementById('heading');
+// a.style.color="red";
+
+// find number of element contining same class
+// let b = document.getElementsByClassName('heading');
+// console.log(b.length);
+
+// how to find number of tag used for a particular tag.
+// console.log(document.getElementsByTagName('h1').length);
