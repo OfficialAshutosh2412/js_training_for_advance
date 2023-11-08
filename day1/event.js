e40f6ba6b0c76d1b9d33373557dfce1a76c8ff30
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-11-08 20:57:20 +0530

    event object

---
+++ day1/event.js
@@ -0,0 +1,41 @@
+// const h2=()=>{
+//     alert("h2");
+// }
+
+// // getting value of an element
+// let four=document.getElementById('four');
+// four.addEventListener('click', ()=>{
+//     alert(four);
+// });
+
+// // click button to change text
+// let btn=document.getElementById('submitBtn');
+// submitBtn.addEventListener('click', ()=>{
+//     submitBtn.innerText="X"
+// });
+
+//event overridding, this method only execute last method.
+// let three=document.getElementById('three');
+// three.onclick= ()=>{
+//     alert('hi')
+// }
+// three.onclick= ()=>{
+//     console.log("hi");
+// }
+// but this will execute all ridden methods
+// let three=document.getElementById('three');
+// three.addEventListener('click',()=>{
+//     alert("hi")
+// });
+// three.addEventListener('click',()=>{
+//     console.log("hi");
+// });
+
+
+// let clickme=document.getElementById('clickme');
+// const checking=()=>{
+//     // console.log(event);
+//     // console.log(event.target);
+//     console.log(event.type);
+// }
+// clickme.addEventListener('click',checking);
