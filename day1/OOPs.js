b73b736afe83ee7ad9ebd63da41c51a3d2e8d727
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-11-14 21:26:34 +0530

    setinterval and tart OOPs(object?)

---
+++ day1/OOPs.js
@@ -0,0 +1,36 @@
+// objects
+// objects is like a school bag, which carries so many things for you.
+// Object is alos a container which caries variable and methods all at once in  a single variable, that object is refered as object.
+
+// method 1 of creating
+// let data={
+//     myname:"ashutosh prasad",
+//     age:22,
+//     department:"IT",
+//     address:"devpur para",
+//     batchNumber:52007,
+//     show:function(){
+//         console.log(`${data.myname}, ${data.age}, ${data.department}, ${data.address}, ${data.batchNumber}`)
+//     }
+// }
+// data.show()
+
+
+// method 2 of creating
+// let nodata={
+//     surname:"Prasad",
+//     showSurname(){
+//         console.log(nodata.surname);
+//     },
+//     fullname:{
+//         firstname:"ashutosh",
+//         lastname:"prasad",
+//         showfullname(){
+//             console.log(`${nodata.fullname.firstname} ${nodata.fullname.lastname}`);
+//         }
+//     }
+// }
+// nodata.fullname.showfullname()
+
+
+// this keyword
