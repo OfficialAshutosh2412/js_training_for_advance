a147e51e1e93761232f57ad471f4d1e131b70f11
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-11-18 21:21:00 +0530

    Object properties

---
+++ day1/Object-Properties.js
@@ -0,0 +1,25 @@
+// object properties
+// it is used to add dynamically add data to object.
+
+// var schoolname="SMS";
+
+// var school={
+//     schoolname:'sms',
+//     myname:"ashu"
+// }
+// console.log(school);
+
+// var school={
+//     [schoolname]:'sms',
+//     myname:"ashu"
+// }
+// console.log(school);
+
+// create empty Object
+// var b={};
+// var c=new Object();
+// add data to empty object
+// b.myname="ashu"
+// c.yourname="bro"
+// console.log(b);
+// console.log(c);
