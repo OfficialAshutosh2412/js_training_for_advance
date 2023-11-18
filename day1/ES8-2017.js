dc5ff1c8d4acfa147f16fc25a77bfecbe354b01c
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2023-11-18 22:09:14 +0530

    es8 feature

---
+++ day1/ES8-2017.js
@@ -0,0 +1,21 @@
+// string padding
+// object.entries
+// object.values
+
+// string padding
+// let a="ashu"
+// let b="ashu".padStart(10);
+// let c="ashu".padEnd(12)+"yeah!"
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// object properties features for looping...
+// let obj1={
+//     myname:"ashu",
+//     mydept:"IT"
+// }
+// retrieve only values
+// console.log(Object.values(obj1));
+// retrieve entries and return the 2D array
+// console.log(Object.entries(obj1));
